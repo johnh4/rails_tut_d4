5d2066ceec70aaa74baf09737baff6b554aab16f
johnh4 <jehowl4@gmail.com>
2013-09-04 17:34:48 -0400

    Add Static Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 RailsTutD4::Application.routes.draw do
+  
+  get "home", to: "static_pages#home"
+  get "help", to: "static_pages#help"
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
+
+  root "static_pages#home"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
